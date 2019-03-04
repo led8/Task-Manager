94a756e317f3c61818fb9daa99e82237b6df3371
Arthur <a.dhuy@orange.fr>
2019-03-04 10:48:46 +0100

    Update commit

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :user
 
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 20 }
   validates :content, presence: true
   validates :progress, presence: true, inclusion: { in: %w[Terminer En\ cours A\ faire] }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-titles = %w[Faire\ les\ courses Aller\ nager Réserver\ billets\ d'avion Cadeau\ noel]
-contents = %w[Lundi Mardi Mercredi Samedi]
-progress = %w[Terminer En\ cours A\ faire]
+# titles = %w[Faire\ les\ courses Aller\ nager Réserver\ billets\ d'avion Cadeau\ noel]
+# contents = %w[Lundi Mardi Mercredi Samedi]
+# progress = %w[Terminer En\ cours A\ faire]
 
 User.destroy_all
 
@@ -8,23 +8,23 @@ puts 'Creating 1 fake user'
 
 user = User.new(
     email: "arthurdhuy@gmail.com",
-    password: "123456",
+    password: "azerty",
     created_at: Time.current
   )
 user.save!
 puts 'Finished create user!'
 
 
-Task.destroy_all
+# Task.destroy_all
 
-puts 'Creating 10 fake tasks'
-4.times do
-  task = Task.new(
-    title: titles.sample,
-    content: contents.sample,
-    progress: progress.sample,
-    user: user
-  )
-  task.save!
-end
-puts 'Finished create tasks!'
+# puts 'Creating 10 fake tasks'
+# 4.times do
+#   task = Task.new(
+#     title: titles.sample,
+#     content: contents.sample,
+#     progress: progress.sample,
+#     user: user
+#   )
+#   task.save!
+# end
+# puts 'Finished create tasks!'
